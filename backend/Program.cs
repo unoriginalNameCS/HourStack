7b67a290bb7dca5dfb75518701afdad430cb1d37
Jeshnil Kumar <kumar_796@hotmail.com>
2026-02-22 05:49:59 +1100

    Added Swagger documentation

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config => {
+    config.DocumentName = "backend";
+    config.Title = "v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+    Console.WriteLine("Swagger: http://localhost:5000/swagger");
+}
 
 app.MapGet("/", () => "Hello World!");
 
